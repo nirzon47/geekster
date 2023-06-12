b613d0613a359a36894d0de601cd0c3db1324ccc
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2023-06-12 21:30:56 +0530

    added page 1, selection sort

---
+++ Module 2/Page 1/SelectionSort.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] nums = new int[N];
+        for (int i = 0; i < N; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (nums[i] > nums[j]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+
+        for (int num : nums) System.out.print(num + " ");
+
+    }
+}
