51a951296d356b42876daf49462311eed3b13b65
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2023-06-12 21:17:13 +0530

    optimised bubble sort

--- Module 2/Page 1/BubbleSort.java
+++ Module 2/Page 1/BubbleSort.java
@@ -5,21 +5,34 @@ public class BubbleSort {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        // Taking the number of elements for the array (N)
         int N = sc.nextInt();
+
         int[] nums = new int[N];
 
+        // Taking input for the array
         for (int i = 0; i < N; i++) {
             nums[i] = sc.nextInt();
         }
 
+        // The First array goes until N - 1 since in a worst case scenario, the last element is always sorted
         for (int i = 0; i < N - 1; i++) {
-            for (int j = 0; j < N - i - 1; j++) {
+            // Setting the limit for j-loop and initializing the flag
+            int flag = 0;
+            int jLimit = N - i - 1;
+
+            for (int j = 0; j < jLimit; j++) {
+                // Standard swapping in case we find an anomaly
                 if (nums[j] > nums[j + 1]) {
                     int temp = nums[j + 1];
                     nums[j + 1] = nums[j];
                     nums[j] = temp;
+                } else {
+                    flag++;
                 }
             }
+            // If the flag is equal to the jLimit, it means all the elements are sorted.
+            if (flag == jLimit) break;
         }
 
         for (int num : nums) System.out.print(num + " ");
